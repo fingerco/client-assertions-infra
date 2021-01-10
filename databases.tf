0afad2ddae621635c03b359a2fa0ea26aa69d746
Cory Finger <cory.j.finger@gmail.com>
2021-01-10 13:42:02 -0800

    Create needed databases

---
+++ databases.tf
@@ -0,0 +1,65 @@
+resource "aws_dynamodb_table" "assertion-failures" {
+  name           = "assertion-failures"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+  range_key      = "failed_at"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "failed_at"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = true
+  }
+}
+
+resource "aws_dynamodb_table" "prediction-failures" {
+  name           = "prediction-failures"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+  range_key      = "failed_at"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "failed_at"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = true
+  }
+}
+
+resource "aws_dynamodb_table" "people" {
+  name           = "people"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "id"
+  range_key      = "created_at"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  attribute {
+    name = "created_at"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = true
+  }
+}
