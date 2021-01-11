d8203f585fd236b043731a3f07578385eaab361e
Cory Finger <cory.j.finger@gmail.com>
2021-01-10 18:59:49 -0800

    Allow IAM role of API to access DynamoDB

--- iam.tf
+++ iam.tf
@@ -21,3 +21,33 @@ EOF
 data "aws_iam_role" "ecs_task_execution_role" {
   name = "ecsTaskExecutionRole"
 }
+
+data "aws_iam_policy_document" "client-assertions-api" {
+  statement {
+    actions = [
+      "dynamodb:*",
+    ]
+
+    resources = [
+      "${aws_dynamodb_table.assertion-failures.arn}",
+      "${aws_dynamodb_table.assertion-failures.arn}/*",
+      "${aws_dynamodb_table.prediction-failures.arn}",
+      "${aws_dynamodb_table.prediction-failures.arn}/*",
+      "${aws_dynamodb_table.people.arn}",
+      "${aws_dynamodb_table.people.arn}/*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "client-assertions-api" {
+  name        = "client-assertions-api"
+  path        = "/"
+  description = "client-assertions-api IAM policy"
+  policy      = data.aws_iam_policy_document.client-assertions-api.json
+}
+
+resource "aws_iam_policy_attachment" "client-assertions-api" {
+  name       = "client-assertions-api-attachment"
+  roles      = [aws_iam_role.client-assertions-api.name]
+  policy_arn = aws_iam_policy.client-assertions-api.arn
+}
